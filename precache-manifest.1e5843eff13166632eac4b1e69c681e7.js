4feaa792aad84241db6131dcba62ff0f7af50bd3
Martin Webster <martin.webster@grifiths-waite.co.uk>
2020-05-26 17:53:18 +0100

    Updates

---
+++ precache-manifest.1e5843eff13166632eac4b1e69c681e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad680c5613023826328b4c962bd6427b",
+    "url": "/mathsclub2/index.html"
+  },
+  {
+    "revision": "f0c9169951b345efda9b",
+    "url": "/mathsclub2/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2d632f4d2a470e2f8d31",
+    "url": "/mathsclub2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0c9169951b345efda9b",
+    "url": "/mathsclub2/static/js/2.1c050090.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mathsclub2/static/js/2.1c050090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d632f4d2a470e2f8d31",
+    "url": "/mathsclub2/static/js/main.a0c7e0af.chunk.js"
+  },
+  {
+    "revision": "01b80a5d041412753ce2",
+    "url": "/mathsclub2/static/js/runtime-main.0a1a17a4.js"
+  }
+]);
